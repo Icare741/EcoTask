35f0b14361e046eb033756d6f7c77f13bb1a107b
Icare741 <enzo.lebuhotel@hotmail.fr>
2025-03-24 06:35:35 +0100

    test: add initial frontend test setup

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,1 +1,10 @@
- 
+import '@testing-library/jest-dom';
+
+// Mock pour matchMedia qui n'est pas disponible dans l'environnement de test
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+}; 
